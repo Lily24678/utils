0ac4623fb7b3a8747e683ab7f0f8ef3a42015c3a
lishanyun <lishanyun@smates.cn>
2020-02-20 09:46:02 +0800

    StringUtils

---
+++ src/main/java/com/lsy/code/utils/StringUtils.java
@@ -0,0 +1,30 @@
+package com.lsy.code.utils;
+
+public class StringUtils {
+	/**
+	 * 判断字符串为空
+	 * @param str
+	 * @return
+	 */
+	public static Boolean isBlank(String str) {
+		if(null==str)return true;
+		if(str.trim().length()==0)return true;
+		return false;
+	}
+	
+	/**
+	 * 判断字符串不为空
+	 * @param str
+	 * @return
+	 */
+	public static Boolean isNotBlank(String str) {
+		if(null!=str&&str.trim().length()>0)return true;
+		return false;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("".trim().length());
+	}
+
+}
