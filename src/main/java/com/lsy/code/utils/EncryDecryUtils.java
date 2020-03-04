c55e2633ce0d9b68f2a66ae8cee4ac985c5b8421
Lily24678 <lishangyun_lsy@163.com>
2020-03-04 17:33:59 +0800

    加解密工具

--- src/main/java/com/lsy/code/utils/EncryDecryUtils.java
+++ src/main/java/com/lsy/code/utils/EncryDecryUtils.java
@@ -9,17 +9,18 @@ import com.thoughtworks.xstream.core.util.Base64Encoder;
  */
 public class EncryDecryUtils {
 
-	public static String getMD5() {
+	public static String getMD5(String str) {
 		String newstr="";
 		try {
 			MessageDigest md5 = MessageDigest.getInstance("MD5");
 			Base64Encoder base64en = new Base64Encoder();
 			//加密后的字符串
-		    newstr = base64en.encode(md5.digest("123".getBytes("utf-8")));
+		    newstr = base64en.encode(md5.digest(str.getBytes("utf-8")));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return newstr;
 	}
+	
 }
